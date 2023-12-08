e7b984ded41c55d91660cd3adbba9377ecc1b1fe
Prakash P <praj85185@gmail.com>
2023-12-08 07:42:57 +0530

    Add RailwayReservationSystem

---
+++ GitHub/RailwayReservationSystem/Train.java
@@ -0,0 +1,13 @@
+public class Train{
+    String name;
+    String time;
+    int passengerStrength;
+    int trainNumber;
+
+    Train(String name, String time, int passengerStrength, int trainNumber) {
+        this.name = name;
+        this.time = time;
+        this.passengerStrength = passengerStrength;
+        this.trainNumber = trainNumber;
+    }
+}
